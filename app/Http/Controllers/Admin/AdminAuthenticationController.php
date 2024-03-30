d08a4beb8de44f4e6ba16baa1bf3e9e3abc412de
Anton Zhary <anton.zhary@gmail.com>
2024-03-30 19:50:16 +0300

    Admin login system.

--- app/Http/Controllers/Admin/AdminAuthenticationController.php
+++ app/Http/Controllers/Admin/AdminAuthenticationController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
 
 class AdminAuthenticationController extends Controller
 {
     public function login()
     {
         return view('admin.auth.login');
+    }
+
+    public function handleLogin(LoginRequest $request)
+    {
+        $request->authenticate();
+
+        return redirect()->route('admin.dashboard');
     }
 }
--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -23,17 +23,24 @@
             <div class="row">
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                     <div class="login-brand">
-                        <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+                        <img src="assets/img/stisla-fill.svg" alt="logo" width="100"
+                             class="shadow-light rounded-circle">
                     </div>
 
                     <div class="card card-primary">
                         <div class="card-header"><h4>Login</h4></div>
 
                         <div class="card-body">
-                            <form method="POST" action="" class="needs-validation" novalidate="">
+                            <form method="POST" action="" class="needs-validation"
+                                  novalidate="">
+                                @csrf
                                 <div class="form-group">
                                     <label for="email">Email</label>
-                                    <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+                                    <input id="email" type="email" class="form-control" name="email" tabindex="1"
+                                           required autofocus>
+                                    @error('email')
+                                    <code>{{ $message }}</code>
+                                    @enderror
                                     <div class="invalid-feedback">
                                         Please fill in your email
                                     </div>
@@ -48,15 +55,19 @@
                                             </a>
                                         </div>
                                     </div>
-                                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                                    <input id="password" type="password" class="form-control" name="password"
+                                           tabindex="2" required>
+                                    @error('password')
                                     <div class="invalid-feedback">
-                                        please fill in your password
+                                        {{ $message }}
                                     </div>
+                                    @enderror
                                 </div>
 
                                 <div class="form-group">
                                     <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
+                                        <input type="checkbox" name="remember" class="custom-control-input" tabindex="3"
+                                               id="remember-me">
                                         <label class="custom-control-label" for="remember-me">Remember Me</label>
                                     </div>
                                 </div>
